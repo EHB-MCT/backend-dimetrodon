09d923f51d9e1af7ce581fb60455af17c3c87275
AachalShrestha <aachal.shrestha@student.ehb.be>
2023-01-23 13:46:45 +0100

    login and register post requests

--- index.js
+++ index.js
@@ -4,30 +4,61 @@ const cors = require('cors');
 const Procedure = require("./procedures.js")
 const on = new Procedure();
 
-
-
 const app = express();
 
-
 app.use(cors())
 app.use(express.json())
 
+app.listen(3001, () => {
+    console.log(`This app is runnning on port ${3001}`)
+});
 
 
+app.post("/login", async (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    let r = await on.login(user.email, user.password);
+    console.log(r[0]);
+    if (!user.email || !user.password) {
+        res.status(404).send({
+            status: "not ok",
+            message: "some fields are missing: email or password"
+        })
+    } else if (r[0].length == 0) {
+        res.status(404).send({
+            status: "not ok",
+            message: "user does not exist"
+        })
+    } else {
+        res.status(200).send({
+            status: "ok",
+            message: "logged in",
+            data: r[0][0]
+        });
+    }
+});
 
+app.post("/register", async (req, res) => {
+    const user = {
+        firstName: req.body.firstname,
+        Lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password
+    };
 
-app.use(cors())
-app.use(express.json())
+    if (await on.findExistingUser(user.email)) {
+        res.status(404).send({
+            status: "not ok",
+            message: "There is already a user with this email"
+        })
+    } else {
+        await on.register(user.firstName, user.Lastname, user.email, user.password);
+        res.status(201).send({
+            status: "ok",
+            message: "user saved",
+        });
+    }
 
-
-
-app.listen(3000, () => {
-    console.log(`This app is runnning on port ${3000}`)
-})
-
-
-app.get("/test", async (req, res) => {
-    let r = await on.params(4, 6);
-
-    res.send(r);
 });
--- procedures.js
+++ procedures.js
@@ -14,6 +14,29 @@ class Procedure {
 
     return result;
   }
+
+  async login(param1, param2) {
+    let result = await sql.awaitQuery("CALL checkUser(?,?)", [param1, param2]);
+    console.log(result);
+    return result;
+  }
+
+  async findExistingUser(param1) {
+    let result = await sql.awaitQuery("CALL findExistingUser(?)", [param1]);
+    if (result[0].length == 0) {
+      return false;
+    } else {
+      return true;
+    }
+
+  }
+
+  async register(firstname, lastname, email, password) {
+    let result = await sql.awaitQuery("CALL createUser(?,?,?,?)", [firstname, lastname, email, password]);
+    return result;
+  }
+
 }
+
 
 module.exports = Procedure;
