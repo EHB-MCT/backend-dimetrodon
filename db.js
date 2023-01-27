2d6a3a7ebd2dbea978fdc7689ee3dbe95d8d70e1
Ali Amrani <ali.amrani@student.ehb.be>
2023-01-27 04:10:21 +0100

    Oops

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql-await");
 // Create a connection to the database
 const connection = mysql.createConnection({
   host: process.env.HOST,
-  user: procces.env.USER,
-  password: procces.env.PASSWORD,
-  database: procces.env.DATABASE
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
 
 // open the MySQL connection
